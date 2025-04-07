21d5745bbae282c45a8c218feccf9f462b4ebad8
nachomartinez3010 <nachomartinez3010@gmail.com>
2025-04-07 15:41:29 -0600

    comment out PRODUCTS_SERVICE export to facilitate NestJS microservices integration with NATS

--- src/config/services.ts
+++ src/config/services.ts
@@ -1,5 +1,6 @@
-export const PRODUCTS_SERVICE = 'PRODUCTS_SERVICE';
+// commented to use nestjs microservices with nats
+/* export const PRODUCTS_SERVICE = 'PRODUCTS_SERVICE';
 export const USERS_SERVICE = 'USERS_SERVICE';
 export const ORDERS_SERVICE = 'ORDERS_SERVICE';
-export const PAYMENTS_SERVICE = 'PAYMENTS_SERVICE';
+export const PAYMENTS_SERVICE = 'PAYMENTS_SERVICE'; */
 export const NATS_SERVICE = 'NATS_SERVICE';

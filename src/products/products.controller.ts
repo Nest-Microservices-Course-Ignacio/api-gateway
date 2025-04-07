e4ed0fb2177a3b5805832b146f8731282148d2b5
nachomartinez3010 <nachomartinez3010@gmail.com>
2025-04-07 15:19:13 -0600

    update NATS configuration in environment template and refactor ProductsController to utilize NATS service for messaging

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,30 +1,30 @@
 import {
-    Body,
-    Controller,
-    Delete,
-    Get,
-    Inject,
-    Logger,
-    Param,
-    Patch,
-    Post,
-    Query,
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  Logger,
+  Param,
+  Patch,
+  Post,
+  Query,
 } from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { catchError, firstValueFrom } from 'rxjs';
 import { ProductsCommands } from 'src/common/cmd/products.cmd';
 import { ActiveRecordsDto } from 'src/common/dto/activeRecords.dto';
-import { PRODUCTS_SERVICE } from 'src/config/services';
+import { NATS_SERVICE } from 'src/config/services';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('products')
 export class ProductsController {
-  constructor(@Inject(PRODUCTS_SERVICE) private productsClient: ClientProxy) {}
+  constructor(@Inject(NATS_SERVICE) private client: ClientProxy) {}
 
   @Get()
   getAllProducts(@Query() pagination: ActiveRecordsDto) {
-    return this.productsClient.send(
+    return this.client.send(
       { cmd: ProductsCommands.FIND_ALL_PRODUCTS },
       pagination,
     );
@@ -34,7 +34,7 @@ export class ProductsController {
   async getProductById(@Param('id') id: string) {
     try {
       const product = await firstValueFrom<{ id: string }>(
-        this.productsClient.send(
+        this.client.send(
           { cmd: ProductsCommands.FIND_ONE_PRODUCT },
           { id },
         ),
@@ -49,7 +49,7 @@ export class ProductsController {
 
   @Post()
   createProduct(@Body() createProductDto: CreateProductDto) {
-    return this.productsClient.send(
+    return this.client.send(
       { cmd: ProductsCommands.CREATE_PRODUCT },
       createProductDto,
     );
@@ -60,7 +60,7 @@ export class ProductsController {
     @Param('id') id: string,
     @Body() updateProductDto: UpdateProductDto,
   ) {
-    return this.productsClient.send(
+    return this.client.send(
       { cmd: ProductsCommands.UPDATE_PRODUCT },
       { id: +id, ...updateProductDto },
     );
@@ -68,7 +68,7 @@ export class ProductsController {
 
   @Delete(':id')
   deleteProduct(@Param('id') id: number) {
-    return this.productsClient
+    return this.client
       .send({ cmd: ProductsCommands.DELETE_PRODUCT }, { id })
       .pipe(
         /* This implementation is for Observables */
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -2,13 +2,13 @@ import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { envs } from 'src/config/envs';
-import { PRODUCTS_SERVICE } from 'src/config/services';
+import { NATS_SERVICE } from 'src/config/services';
 
 @Module({
   imports: [
     ClientsModule.register([
       {
-        name: PRODUCTS_SERVICE,
+        name: NATS_SERVICE,
         transport: Transport.NATS,
         options: {
           servers: envs.natsServers,

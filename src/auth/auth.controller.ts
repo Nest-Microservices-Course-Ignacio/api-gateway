2243e29848664f7bb066501542dbe6aaecedfd75
nachomartinez3010 <nachomartinez3010@gmail.com>
2025-04-21 19:09:46 -0600

    feat(auth): add DTOs for user registration and login; update AuthController to use typed data

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,18 +2,20 @@ import { Body, Controller, Inject, Post } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { AuthCommands } from 'src/common/cmd/auth.cmd';
 import { NATS_SERVICE } from 'src/config/services';
+import { RegisterDTO } from './dto/register.dto';
+import { LoginUserDTO } from './dto/loginUser.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(@Inject(NATS_SERVICE) private client: ClientProxy) {}
 
   @Post('register')
-  setRegisterUser(@Body() data: any) {
+  setRegisterUser(@Body() data: RegisterDTO) {
     return this.client.send({ cmd: AuthCommands.AUTH_REGISTER_USER }, data);
   }
 
   @Post('login')
-  setLoginUser(@Body() data: any) {
+  setLoginUser(@Body() data: LoginUserDTO) {
     return this.client.send({ cmd: AuthCommands.AUTH_LOGIN_USER }, data);
   }
 
